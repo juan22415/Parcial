985d5681f035b7a8b1627465d84604220ba8577c
juan camilo rios <juan22415@hotmail.com>
2017-05-23 12:36:15 -0500

    puntos 8 y 9

--- Assets/Collect.cs
+++ Assets/Collect.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collect : MonoBehaviour {
+public class Collect : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Generate generate;
+    public GameObject generator;
+    // Use this for initialization
+    void Start()
+    {
+        generator = GameObject.Find("Crystalsmaager");
+        generate = generator.GetComponent<Generate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
-            Destroy(gameObject);
-       
+        generate.Generatenewcrystal();
+        Destroy(gameObject);
+
     }
 
 
--- Assets/Generate.cs
+++ Assets/Generate.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Generate : MonoBehaviour {
+public class Generate : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private GameObject crystal;
+    private Transform transform;
+
+    public int currentcrystas;
+
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    public void Generatenewcrystal()
+    {
+        Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-5,5), 0);
+        Debug.Log(position);
+        transform.position = position;
+        setcurrentcrystals();
+        Instantiate(crystal,transform);
+    }
+
+    private void setcurrentcrystals()
+    {
+        currentcrystas++;
+
+        if (currentcrystas > 5)
+        {
+            Debug.Log("ganaste");
+        }
+    }
 }
