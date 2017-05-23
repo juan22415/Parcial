45e2321671fa359eacc190dcf8b59f2a201714f0
juan camilo rios <juan22415@hotmail.com>
2017-05-23 12:54:34 -0500

    medio punto 10

---
+++ Assets/enemystates.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemystates : MonoBehaviour {
+
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        rigidbody.velocity=(new Vector2 (-5, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            rigidbody.velocity = (new Vector2(-1*rigidbody.velocity.x, 0));
+        }
+    }
+}
